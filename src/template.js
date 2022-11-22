f2d28f7831e615c130f740b749fa6f03813d89cc
Dominic <dompaulazzo@gmail.com>
2022-11-22 11:46:07 -0800

    adding the generate intern section to the template.js page

--- src/template.js
+++ src/template.js
@@ -37,3 +37,22 @@ const generateEngineer = engineer => {
 </div>
         `;
 };
+ 
+// create the html for interns
+ const generateIntern = intern => {
+    return `
+<div class="card employee-card">
+<div class="card-header bg-primary text-white">
+    <h2 class="card-title">${intern.getName()}</h2>
+    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+</div>
+<div class="card-body">
+    <ul class="list-group">
+        <li class="list-group-item">ID: ${intern.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item">School: ${intern.getSchool()}</li>
+    </ul>
+</div>
+</div>
+    `;
+};
