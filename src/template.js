d8ff3e9853158019d42a2006b09891bf8106845e
Dominic <dompaulazzo@gmail.com>
2022-11-22 11:13:48 -0800

    adding the engineers portion to the template.js page

--- src/template.js
+++ src/template.js
@@ -1,1 +1,39 @@
 const generateTeam = team => {
+
+        // create the manager html
+const generateManager = manager => {
+        return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+};
+
+    // create the html for engineers
+const generateEngineer = engineer => {
+    return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+};
