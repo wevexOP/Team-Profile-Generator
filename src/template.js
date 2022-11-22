24b4eddfd354d836b5002335529d4e42373c77e5
Dominic <dompaulazzo@gmail.com>
2022-11-22 12:31:07 -0800

    adding the html for the landing page of the project.

--- src/template.js
+++ src/template.js
@@ -56,3 +56,62 @@ const generateEngineer = engineer => {
 </div>
     `;
 };
+
+const html = [];
+
+html.push(team
+    .filter(employee => employee.getRole() === "Manager")
+    .map(manager => generateManager(manager))
+);
+html.push(team
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => generateEngineer(engineer))
+    .join("")
+);
+html.push(team
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => generateIntern(intern))
+    .join("")
+);
+
+return html.join("");
+
+}
+
+// export function to generate entire page
+module.exports = team => {
+
+return `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>My Team</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+            <h1 class="text-center text-white">My Team</h1>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="row team-area col-12 d-flex justify-content-center">
+            ${generateTeam(team)}
+        </div>
+    </div>
+</div>
+</body>
+</html>
+`;
+};
