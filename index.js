411e8ab30b12c938846f34d8c81610a2a7a86dba
Dominic <dompaulazzo@gmail.com>
2022-11-23 08:02:40 -0800

    got the npm to work but I can not get my node index.js to run yet.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
-const inquirer = require("inquirer");
+const inquirer = require('inquirer')
 const path = require("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
